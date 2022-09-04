88220e778ecff7a9dc43f2012a61385de6e79a3d
kris-bond <21047155+kris-bond@users.noreply.github.com>
2022-09-04 16:07:32 +0100

    formatting data

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
 const PORT = 8000;
 
-const axios = require('axios')
-const express = require('express')
+const axios = require('axios');
+const express = require('express');
 
 const app = express();
 
-const url = 'https://en.wikipedia.org/w/index.php?title=2022%E2%80%9323_Premier_League&action=edit'
+const urls = [];
 
-axios(url)
-    .then(response => {
-        const html = response.data
+const url21to22 = 'https://en.wikipedia.org/w/index.php?title=2021%E2%80%9322_Premier_League&action=edit';
+const url22to23 = 'https://en.wikipedia.org/w/index.php?title=2022%E2%80%9323_Premier_League&action=edit';
 
-        const names = []
-        const results = []
+urls.push(url21to22, url22to23);
 
-        html.split('\n').forEach(function(data) {
+urls.forEach(url => {
 
-            if(data.startsWith("| name_")){
-                names.push(data);
-            }
+    axios(url)
+        .then(response => {
+            const html = response.data;
+
+            //formatting for year from url
+            season = url.substring(43, 47) + "/" + url.substring(56, 58);
+
+            const names = [];
+            const results = [];
+
+            html.split('\n').forEach(function(data) {
+
+                if(data.startsWith("| name_")){
+                    names.push(data);
+                }
+
+                if(data.startsWith("| match_")){
+                    results.push(data);
+                }
+            });
+
+            //Deduplicate names
+            // names.forEach(function(name) {
+                //console.log(name.substring(0, 10));
+            // });
+
+            //console.log(season, names, results)
+            console.log(names)
+
+            // output to json
+            const json = JSON.stringify(season, names, results);
+
+            // var fs = require('fs');
+            // fs.readFile('data.json',function(err,json){
+            //     if(err) throw err;
+            //     //var parseJson = JSON.parse(content);
+            //     fs.writeFile('data.json',JSON.stringify(json),function(err){
+            //         if(err) throw err;
+            //     })
+            // })
             
-            if(data.startsWith("| match_")){
-                results.push(data);
-            }
-        });
+        })
 
-        console.log(names, results)
-        
-    })
+})
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
